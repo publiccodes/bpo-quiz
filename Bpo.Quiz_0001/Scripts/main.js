76f24f455be5a87afd5dbb5b749f655cfefa1fcb
saitou <shusaku@abox6.so-net.ne.jp>
2014-09-18 04:57:11 +0900

    最初

---
+++ Bpo.Quiz_0001/Scripts/main.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    $("#button_01").hover(function () {
+        $("#q01_img").css("background-position", "0px -500px");
+    }, function () {
+        $("#q01_img").css("background-position", "0px 0px");
+    });
+    $("#button_01").click(function () {
+        playSound("click_sound");
+    });
+    $("#button_01").mouseover(function () {
+        playSound("hover_sound");
+    });
+});
+
+function playSound(id) {
+    document.getElementById(id).load();
+    document.getElementById(id).play();
+}
